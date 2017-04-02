917852f08e849cd543ed75e0d7ffc57b519c877d
Ayun <38136937@qq.com>
2017-04-02 21:45:46 +0800

    add adtabase

--- hello.py
+++ hello.py
@@ -1,40 +1,79 @@
-
+import os
 from flask import Flask
-from flask import render_template
-from flask_bootstrap import Bootstrap
+from flask import render_template      
+from flask_bootstrap import Bootstrap   # 导#入bootstrap 前端框架
+
+from flask_script import Manager   # manager命令行模式
+from flask_moment import Moment   #
 
 from flask_wtf import FlaskForm     # 导入扩展模块
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required      # 验证函数，确保字段不为空
 
-from flask import session, redirect, url_for, flash
+from flask import session, redirect, url_for, flash    # 
+
+from flask_sqlalchemy import SQLAlchemy      # sqlalchemy数据库框架
+
+
+# 数据库在本机的路径
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-'''
-@app.route('/')
-def index():
-    response = make_response('<h1> this document carries a cookie! </h1>')
-    response.set_cookie('answer', '42')
-    return response
-'''
+# 配置一个SQLite数据库
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWM'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+# 初始化数据库 
+db = SQLAlchemy(app)
+
+# 初始化前端框架
 bootstrap = Bootstrap(app)
 
+# 初始化manager模式
+manager = Manager(app)
 
-'''
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-'''
+# 定义时间
+moment = Moment(app)
 
 
+
+#定义Role 和 User 模型
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique = True)
+	users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique = True, index = True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+
+# 表单类
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [Required()])     # 用户可以输入内容的文本框，值被name变量接收
     submit = SubmitField('Submit')     # 提交按钮
 
+
+
+
+#定义页面
 @app.route('/', methods = ['GET', 'POST'])     #告诉Flask做URL映射中把这个视图函数注册为GET和POST请求的处理程序
 def index():
     form = NameForm()
@@ -53,5 +92,10 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug =True)
+	# 调试模式
+ #   app.run(debug =True)
 
+    #命令行模式
+    manager.run()
+
+    
